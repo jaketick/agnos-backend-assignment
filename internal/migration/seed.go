432ebf663ef39a48de23e6024cfbbdcaecb41925
Jake <sahatsawat.fangsen@gmail.com>
2026-09-19 17:16:23 +0700

    for seed staff

--- internal/migration/seed.go
+++ internal/migration/seed.go
@@ -25,5 +25,27 @@ func Seed(db *gorm.DB, cfg *config.Config) error {
 		return fmt.Errorf("failed to seed hospital: %w", err)
 	}
 
+	/* passwordHash, err := bcrypt.GenerateFromPassword(
+		[]byte("password123"),
+		bcrypt.DefaultCost,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to hash seed staff password: %w", err)
+	}
+
+	staff := model.Staff{
+		Username:     "staff01",
+		PasswordHash: string(passwordHash),
+		HospitalID:   hospital.ID,
+	}
+
+	err = db.
+		Where("username = ? AND hospital_id = ?", staff.Username, staff.HospitalID).
+		FirstOrCreate(&staff).
+		Error
+	if err != nil {
+		return fmt.Errorf("failed to seed staff: %w", err)
+	} */
+
 	return nil
 }
